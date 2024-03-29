f7b67363c02bda060918ed1c1de62a9ef41e3df1
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-29 10:27:33 -0300

    read csv

--- lib/reports.ex
+++ lib/reports.ex
@@ -4,15 +4,14 @@ defmodule Reports do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Reports.hello()
-      :world
-
+  WIP
   """
-  def hello do
-    :world
+  def build(filename) do
+    "reports/inputs/#{filename}"
+    |> File.read()
+    |> handle_file()
   end
+
+  defp handle_file({:ok, result}), do: result
+  defp handle_file({:error, _reason}), do: {:error, "Error while reading the file"}
 end
