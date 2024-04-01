8c9a5e0416d586d40b3be0876cb2a4fef8d81bc9
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-01 18:21:24 -0300

    new feat

--- lib/reports.ex
+++ lib/reports.ex
@@ -3,6 +3,17 @@ defmodule Reports do
   Documentation for `Reports`.
   """
 
+  @available_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pizza",
+    "sushi",
+    "pastel",
+    "prato_feito"
+  ]
+
   @doc """
   WIP
   """
@@ -27,7 +38,23 @@ defmodule Reports do
     |> List.update_at(2, &String.to_integer/1)
   end
 
-  defp reports_acc(), do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+  defp reports_acc() do
+    foods = Enum.into(@available_foods, %{}, &{&1, 0})
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 
-  defp sum_values([id, _food_name, price], acc), do: Map.put(acc, id, acc[id] + price)
+    %{
+      "foods" => foods,
+      "users" => users
+    }
+  end
+
+  defp sum_values([id, food_name, price], %{"users" => users, "foods" => foods}) do
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+    users = Map.put(users, id, users[id] + price)
+
+    %{
+      "foods" => foods,
+      "users" => users
+    }
+  end
 end
