056b6828c41302c42c6a1474be2ca71f5fa2753e
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-03 19:25:11 -0300

    parallelism report generation

--- lib/reports.ex
+++ lib/reports.ex
@@ -23,9 +23,25 @@ defmodule Reports do
     |> Enum.reduce(reports_acc(), &sum_values/2)
   end
 
+  def build_many(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(reports_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+  end
+
   def most_spent_user(%{"users" => users}), do: Enum.max_by(users, fn {_k, v} -> v end)
 
   def best_selling_food(%{"foods" => foods}), do: Enum.max_by(foods, fn {_k, v} -> v end)
+
+  defp sum_reports(
+         %{"foods" => prev_foods, "users" => prev_users},
+         %{"foods" => foods, "users" => users}
+       ) do
+    res_foods = Map.merge(prev_foods, foods, fn _k, v1, v2 -> v1 + v2 end)
+    res_users = Map.merge(prev_users, users, fn _k, v1, v2 -> v1 + v2 end)
+
+    %{"foods" => res_foods, "users" => res_users}
+  end
 
   defp parse_file(filename) do
     "reports/inputs/#{filename}"
