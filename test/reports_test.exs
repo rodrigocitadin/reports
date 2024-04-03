26647bcdbd255f38aa962bd8e5fbb6047aba0aa1
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-03 20:00:45 -0300

    tests and finish

--- test/reports_test.exs
+++ test/reports_test.exs
@@ -57,6 +57,61 @@ defmodule ReportsTest do
     end
   end
 
+  describe "build_many/1" do
+    test "just build the report" do
+      response =
+        ["report_test.csv", "report_test.csv"]
+        |> Reports.build_many()
+
+      expected_response = %{
+        "foods" => %{
+          "açaí" => 2,
+          "churrasco" => 4,
+          "esfirra" => 6,
+          "hambúrguer" => 4,
+          "pastel" => 0,
+          "pizza" => 4,
+          "prato_feito" => 0,
+          "sushi" => 0
+        },
+        "users" => %{
+          "1" => 96,
+          "10" => 72,
+          "11" => 0,
+          "12" => 0,
+          "13" => 0,
+          "14" => 0,
+          "15" => 0,
+          "16" => 0,
+          "17" => 0,
+          "18" => 0,
+          "19" => 0,
+          "2" => 90,
+          "20" => 0,
+          "21" => 0,
+          "22" => 0,
+          "23" => 0,
+          "24" => 0,
+          "25" => 0,
+          "26" => 0,
+          "27" => 0,
+          "28" => 0,
+          "29" => 0,
+          "3" => 62,
+          "30" => 0,
+          "4" => 84,
+          "5" => 98,
+          "6" => 36,
+          "7" => 54,
+          "8" => 50,
+          "9" => 48
+        }
+      }
+
+      assert response == expected_response
+    end
+  end
+
   describe "most_spent_user/1" do
     test "just work" do
       response =
