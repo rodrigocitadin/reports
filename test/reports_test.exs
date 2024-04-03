4db530761b9b929856d0ffea367f2176b05539b3
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-03 17:13:33 -0300

    test build

--- test/reports_test.exs
+++ test/reports_test.exs
@@ -2,7 +2,64 @@ defmodule ReportsTest do
   use ExUnit.Case
   doctest Reports
 
-  test "greets the world" do
-    assert Reports.hello() == :world
+  describe "build/1" do
+    test "just build the report" do
+      response =
+        "report_test.csv"
+        |> Reports.build()
+
+      expected_response = %{
+        "foods" => %{
+          "açaí" => 1,
+          "churrasco" => 2,
+          "esfirra" => 3,
+          "hambúrguer" => 2,
+          "pastel" => 0,
+          "pizza" => 2,
+          "prato_feito" => 0,
+          "sushi" => 0
+        },
+        "users" => %{
+          "1" => 48,
+          "10" => 36,
+          "11" => 0,
+          "12" => 0,
+          "13" => 0,
+          "14" => 0,
+          "15" => 0,
+          "16" => 0,
+          "17" => 0,
+          "18" => 0,
+          "19" => 0,
+          "2" => 45,
+          "20" => 0,
+          "21" => 0,
+          "22" => 0,
+          "23" => 0,
+          "24" => 0,
+          "25" => 0,
+          "26" => 0,
+          "27" => 0,
+          "28" => 0,
+          "29" => 0,
+          "3" => 31,
+          "30" => 0,
+          "4" => 42,
+          "5" => 49,
+          "6" => 18,
+          "7" => 27,
+          "8" => 25,
+          "9" => 24
+        }
+      }
+
+      assert response == expected_response
+    end
+  end
+
+  describe "most_spent_user/1" do
+  end
+
+  describe "best_selling_food/1" do
   end
 end
